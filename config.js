f57d0a7a0bdbd380444bea24b2be5c26b461f0ef
Kaidonb <86923357+Kaidonb@users.noreply.github.com>
2022-07-28 17:41:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Peo à!',
+    introTitle: 'Cúc à! 🌼🌼🌼',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Hẹn em một ngày không xa anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tu.tai.nb/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
