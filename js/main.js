8db6c9d531b94d9d069edefeb081c60fa782b066
Kaidonb <86923357+Kaidonb@users.noreply.github.com>
2022-07-29 00:36:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/cuteCat.jpg',
+        imageUrl: 'img/cuc.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
