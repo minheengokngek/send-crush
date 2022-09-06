cf9d712ff823edf8e0a4e064441783f8f63ea075
tanacouple <112962785+tanacouple@users.noreply.github.com>
2022-09-06 22:14:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Cúc à! 🌼🌼🌼',
+    introTitle: 'Ngọc Anh à! 🍒🍒🍒',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích Tài <33',
+    btnNo: 'Không, Tài à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Ngọc Anh lại thích tớ <3',
+    btnReply: 'Gửi cho tớ <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hẹn em một ngày không xa anh sang Đức cùng với em nhó <3',
+    mess: 'Tớ biết mà 🥰. Yêu Ngọc Anh nhiều nhiều 😘😘',
+    messDesc: 'Mình cùng nhau đi du lịch khắp thế giới nhó <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/tu.tai.nb/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
